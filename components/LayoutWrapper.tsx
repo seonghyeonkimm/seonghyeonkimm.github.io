662bfbafd0f54a4f675956426e0a2ea7a4f396ce
demian <demian@kcd.co.kr>
2022-05-05 18:14:14 +0900

    Change logo width

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -21,7 +21,7 @@ const LayoutWrapper = ({ children }: Props) => {
             <Link href="/" aria-label={siteMetadata.headerTitle}>
               <div className="flex items-center justify-between">
                 <div className="mr-3">
-                  <Logo style={{ width: 54 }} />
+                  <Logo style={{ width: 40 }} />
                 </div>
                 {typeof siteMetadata.headerTitle === 'string' ? (
                   <div className="hidden h-6 text-2xl font-semibold sm:block">
