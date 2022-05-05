2974cada2c3fbd2126f1aca184b6839c195eb37c
demian <demian@kcd.co.kr>
2022-05-05 14:10:33 +0900

    Update headerNavLink

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,7 +1,8 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
+  // TODO: 나중에 추가
+  // { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
 ]
 
