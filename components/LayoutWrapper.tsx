5c8a17fdbbcde86b961e8d28bb3ef81a083d3a5e
demian <demian@kcd.co.kr>
2022-05-05 14:08:19 +0900

    Change logo icon

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -21,7 +21,7 @@ const LayoutWrapper = ({ children }: Props) => {
             <Link href="/" aria-label={siteMetadata.headerTitle}>
               <div className="flex items-center justify-between">
                 <div className="mr-3">
-                  <Logo />
+                  <Logo style={{ width: 54 }} />
                 </div>
                 {typeof siteMetadata.headerTitle === 'string' ? (
                   <div className="hidden h-6 text-2xl font-semibold sm:block">
